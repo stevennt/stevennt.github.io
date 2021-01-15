66a7dc30c28df640071a04e1e369ad3e77f61bf0
Sourav <srvz39@gmail.com>
2021-01-15 21:27:59 +0530

    captcha implemented

---
+++ app.js
@@ -0,0 +1,81 @@
+  // Set the configuration for your app
+  // TODO: Replace with your project's config object
+  //   var config = {
+  //     apiKey: "AIzaSyA-4K0RxAzan8wI4QJi5xmIVJrJ2S1F7zE",
+  //     authDomain: "abnasiatest.firebaseapp.com",
+  //     databaseURL: "https://abnasiatest-default-rtdb.firebaseio.com",
+  //     storageBucket: "abnasiatest.appspot.com"
+  //   };
+  //   firebase.initializeApp(config);
+
+  // Get a reference to the database service
+  var database = firebase.database();
+
+  var school_data = database.ref('abnasiatest-default-rtdb/schools_data')
+  school_data.on('value', (snapshot) => {
+      console.log(snapshot.val())
+  })
+
+  function lookup() {
+
+
+
+      var school_id = document.getElementById('schoolID').value.trim()
+      var student_id = document.getElementById('studentID').value.trim()
+
+      console.log(school_id, student_id)
+      if (school_id.length > 0 && student_id.length > 0) {
+          if (grecaptcha.getResponse().length > 0) {
+              
+              school_data.orderByChild('school_id').equalTo(school_id).once('value', function (snapshot) {
+                  // console.log(snapshot.val())
+                  school_records = snapshot.val()
+                  students = school_records[school_id]['students']
+                  // console.log(school_records)
+                  for (i = 0; i < students.length; i++) {
+                      if (students[i]['student_id'] == student_id) {
+                          console.log(students[i])
+
+                          document.getElementById('lf_school_id').innerText = school_records[school_id]['school_id']
+                          document.getElementById('lf_school_name').innerText = school_records[school_id]['school_name']
+                          document.getElementById('lf_student_id').innerText = students[i]['student_id']
+                          document.getElementById('lf_student_name').innerText = students[i]['student_name']
+                          document.getElementById('lf_balance').innerText = students[i]['outstanding_balance']
+                          document.getElementById('lf_balance_date').innerText = school_records[school_id]['balance_date']
+
+                          if (document.getElementById('balancediv').style.display = "none") {
+                              document.getElementById('balancediv').style.display = "block"
+                          }
+
+                      }
+                  }
+              })
+          } else {
+              alert('Check the CAPTCHA!')
+          }
+      } else {
+          alert('Both fields are mandatory!')
+      }
+  }
+
+  //   school_data.orderByValue().on("value", function (data) {
+
+  //       data.forEach(function (data) {
+  //           console.log("The " + data.key + " rating is " + data.val());
+  //       });
+
+  //   });
+
+  //   school_data.orderByChild('school_id').on("value", (data) => {
+  //       data.forEach(function (data) {
+  //           console.log(data, data.val())
+  //       })
+  //   })
+
+  //   rtdb_url = "https://abnasiatest-default-rtdb.firebaseio.com/abnasiatest-default-rtdb/schools_data.json"
+
+  // //   fetch(rtdb_url).then(function (data) {
+  // //       return data.json()
+  // //   }).then(function (data) {
+  // //       console.log(data)
+  // //   })
