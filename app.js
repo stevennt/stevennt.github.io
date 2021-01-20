055f8df6d10bd8939ac6006098d52565fc9fa0ef
Son Nguyen <steven.n.t@outlook.com>
2021-01-20 19:35:42 +0700

    To Lower-case.

--- app.js
+++ app.js
@@ -24,7 +24,8 @@
       if (school_id.length > 0 && student_id.length > 0) {
           //if (grecaptcha.getResponse().length > 0) {
 
-              school_data.orderByChild('school_id').equalTo(school_id).once('value', function (snapshot) {
+              //school_data.orderByChild('school_id').equalTo(school_id).once('value', function (snapshot) {
+              school_data.orderByChild('school_id').toLowerCase().equalTo(school_id).once('value', function (snapshot) {
                   // console.log(snapshot.val())
                   school_records = snapshot.val()
                   if (school_records == null) {
@@ -33,7 +34,9 @@
                       students = school_records[school_id]['students']
                       // console.log(school_records)
                       for (i = 0; i < students.length; i++) {
-                          if (students[i]['student_id'] === student_id) {
+                          //if (students[i]['student_id'] === student_id) {
+                          if (toLowerCase(students[i]['student_id']) === toLowerCase(student_id)) {
+                            
                               console.log(students[i])
                               studentFound = true
                               document.getElementById('lf_school_id').innerText = school_records[school_id]['school_id']
